b183ec53b9579fa7a17f1e9462941e5072219078
_hange <52858353+onur-py@users.noreply.github.com>
2021-10-01 15:02:10 +0300

    Update riemann_integral.py

--- riemann_integral.py
+++ riemann_integral.py
@@ -12,13 +12,13 @@ b=int(input("upper limit: "))
 #Riemann sums
 for n in range(1,pow(10,9)):#As n approaches infinity
 	delta_x=(b-a)/n
-	xi=0
+	val=0
 	for i in range(0,n):
-		xi+=func(a+i*delta_x)*delta_x
+		val+=func(a+i*delta_x)*delta_x
 
-	print(xi)
+	print(val)
 #Rieman sums
 
 #result
 os.system("cls")
-print(round(xi,2))#Approximate Value
+print(round(val,2))#Approximate Value
